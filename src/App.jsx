1fa8fd929faf832d3f8b25ed599cb8e37ea0e9ed
Yanal-Odeh <Oudeh.yanal03@gmail.com>
2025-10-06 13:45:17 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './Pages/Home/Home'
+import About from './Pages/About/About'
+import Root from './routes/Root';
+import NotFound from './Pages/NotFound/NotFound';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  { 
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+       {
+        path: "/about",
+        element: <About />
+      },
+    
+      {
+        path: "*",
+        element: <NotFound />
+      }
+      
+    ]
+  },
+]);
+
+function App() {
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import './Navbar.module.scss'
+function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
